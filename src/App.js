a3a91be218eaa86165a5f0bfa5d0f2bbf26b6b17
Maydelene <huaweimaydelene@gmail.com>
2025-07-04 15:26:58 +0800

    new file:   .gitignore 	new file:   README.md 	deleted:    css/styles.css 	deleted:    home.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/components/Squares.css 	new file:   src/components/Squares.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Squares from './components/Squares';
+
+function App() {
+  return (
+    <div className="App">
+      <Squares />
+      <header>
+        <h1>Hi, I'm Maydelene Chavez</h1>
+        <p>Web Developer | Designer | Learner</p>
+      </header>
+      {/* other sections */}
+    </div>
+  );
+}
+
+export default App;
